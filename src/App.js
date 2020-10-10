679e6a92eb1e5c8148b63d85f7e32fcbf359e5db
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-10-10 12:45:16 +1000

    Styling Background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 
 function App() {
   const [message, setMessage] = React.useState("JavaScript is so cool.");
@@ -8,40 +9,13 @@ function App() {
   }
 
   return (
+    <div className="app">
       <div>
           <h1>{message}</h1>
           <button onClick={handleClick}>Update the message!</button>
+      </div>
       </div>
   );
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
